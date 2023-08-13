1bf91e25113e54431ffabd3a83d0bddbb09b8616
valeenferreyra <vfe.2001@gmail.com>
2023-08-13 07:25:25 -0300

    Nuevo ecommerce de SuperMarket

---
+++ js/productos.js
@@ -0,0 +1,29 @@
+const productos = [{
+    id: 1,
+    img: "https://carrefourar.vtexassets.com/arquivos/ids/205215-1200-auto?v=637601556417500000&width=1200&height=auto&aspect=true",
+    nombre: "Harina",
+    precio: 50,
+    cantidad: 1,
+},
+{
+    id: 2,
+    img: "https://media.f2h.shop/media/catalog/product/cache/0c018cd5b914260961bff118ae463f9e/t/e/terrabusi.png",
+    nombre: "Galletitas",
+    precio: 100,
+    cantidad: 1,
+},
+{
+    id: 3,
+    img: "https://dislicoresqa.vtexassets.com/arquivos/ids/230453/370526-Stela-artois-lata-269-X6.png?v=637883595311270000",
+    nombre: "Cerveza",
+    precio: 150,
+    cantidad: 1,
+},
+{
+    id: 4,
+    img: "https://www.laserenisima.com.ar/images/productos/2044372Gdelargaroja.png",
+    nombre: "Leche",
+    precio: 200,
+    cantidad: 1,
+},
+];
